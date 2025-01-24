92e14af718bf643e45f804172d19db61150d721c
Nasim <nasim.hossain@inceptasolutions.com>
2025-01-24 22:22:34 +0600

    Add .gitignore to exclude node_modules

---
+++ dist/controllers/user-controller.js
@@ -0,0 +1,26 @@
+import User from "../models/User.js";
+import { hash } from "bcrypt";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "ok", users });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "error", error });
+    }
+};
+export const userSignup = async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        return res.status(200).json({ message: "ok", id: user._id.toString });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(200).json({ message: "error", error });
+    }
+};
+//# sourceMappingURL=user-controller.js.map
---
+++ src/utils/validator.ts
@@ -0,0 +1,7 @@
+import { body } from "express-validator";
+
+const signupValidator=[
+    body("name").notEmpty().withMessage("Name is required"),
+    body("password").trim().isLength({min : 8}).withMessage("Password Should Contain Atleast 8 characters"),
+    body("email").trim().isEmail().withMessage("Email is required"),
+]

f1537c7c415c03b463a976cccffe0d442b6f13b1
Nasim <nasim.hossain@inceptasolutions.com>
2025-04-18 16:15:02 +0600

    Continuing After 2 Months

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,5 +1,23 @@
-import { body } from "express-validator";
+import { NextFunction, Request, Response } from "express";
+import { body, ValidationChain, validationResult } from "express-validator";
 
+
+const validate=(validations:ValidationChain[])=>{
+    return async (req: Request,res:Response,next:NextFunction)=>{
+        for(let validation of validations){
+            const result=await validation.run(req);
+            if (!result.isEmpty()) {
+                return res.status(400).json({ message: result.array()[0].msg })
+            }
+        }
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return next()
+        }
+        res.status(400).json({ message: "Invalid Input" })
+    };
+};
 const signupValidator=[
     body("name").notEmpty().withMessage("Name is required"),
     body("password").trim().isLength({min : 8}).withMessage("Password Should Contain Atleast 8 characters"),

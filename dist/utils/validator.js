de776c80a271582d7d4e9f6901805d4e5a960c13
Nasim <nasim.hossain@inceptasolutions.com>
2025-04-23 14:40:07 +0600

    Validator Added

--- dist/utils/validator.js
+++ dist/utils/validator.js
@@ -1,4 +1,19 @@
-import { body } from "express-validator";
+import { body, validationResult } from "express-validator";
+const validate = (validations) => {
+    return async (req, res, next) => {
+        for (let validation of validations) {
+            const result = await validation.run(req);
+            if (!result.isEmpty()) {
+                return res.status(400).json({ message: result.array()[0].msg });
+            }
+        }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return next();
+        }
+        res.status(400).json({ message: "Invalid Input" });
+    };
+};
 const signupValidator = [
     body("name").notEmpty().withMessage("Name is required"),
     body("password").trim().isLength({ min: 8 }).withMessage("Password Should Contain Atleast 8 characters"),
